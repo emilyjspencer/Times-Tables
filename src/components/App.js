348d90056c32b8118516b69e4c5d1aa0c8ef5f1f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-07 15:29:52 +0100

    Transpiles SCSS to CSS

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Learn you times tables with Barbie</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
